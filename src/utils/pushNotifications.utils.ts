90cf7ca1ee056de222c2531af783c830b052e19e
Guy-Jacques Isombe <isombeg@mcmaster.ca>
2023-04-07 14:34:39 -0400

    eliminating comments

--- src/utils/pushNotifications.utils.ts
+++ src/utils/pushNotifications.utils.ts
@@ -2,29 +2,6 @@ import Expo from 'expo-server-sdk';
 import { Model } from 'mongoose';
 import { PushTokenRegistrationDocument } from 'src/models/push/schemas/pushTokenRegistration.schema';
 
-// export const sendNotification = async (
-//   expo: Expo,
-//   title: string,
-//   token: string,
-//   body: string,
-//   data: Object,
-// ) => {
-//   const message = {
-//     to: token,
-//     body: body,
-//     data: data,
-//   };
-//
-//   const chunks = expo.chunkPushNotifications([message]);
-//   for (const chunk of chunks) {
-//     try {
-//       await expo.sendPushNotificationsAsync(chunk);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-// };
-
 const SEND_NOTIF_URL = "https://exp.host/--/api/v2/push/send"
 const GET_RECEIPT_URL = "https://exp.host/--/api/v2/push/getReceipts"
 
